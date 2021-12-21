04bc10132e17c823ede78635270b133f0f857453
Marvin Buss <marvin.buss@gmail.com>
2021-12-21 14:51:37 +0100

    added role metaclass for purview role assignments

---
+++ code/PurviewAutomation/Models/Purview/Role.cs
@@ -0,0 +1,27 @@
+﻿namespace PurviewAutomation.Models.Purview
+{
+    internal enum Role
+    {
+        CollectionAdministrator,
+        DataSourceAdministrator,
+        DataCurator,
+        DataReader,
+        PolicyAuthor
+    }
+
+    internal static class RoleConverter
+    {
+        internal static string ConvertRoleToString(Role role)
+        {
+            return role switch
+            {
+                Role.CollectionAdministrator => "purviewmetadatarole_builtin_collection-administrator",
+                Role.DataSourceAdministrator => "purviewmetadatarole_builtin_data-source-administrator",
+                Role.DataCurator => "purviewmetadatarole_builtin_data-curator",
+                Role.DataReader => "purviewmetadatarole_builtin_purview-reader",
+                Role.PolicyAuthor => "purviewmetadatarole_builtin_policy-author",
+                _ => null,
+            };
+        }
+    }
+}
