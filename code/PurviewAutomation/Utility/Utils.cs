2eb7e8919a16ec0dcc418ee075c01440cce6405b
Marvin Buss <marvin.buss@gmail.com>
2021-12-21 14:50:23 +0100

    added utils classes

---
+++ code/PurviewAutomation/Utility/Utils.cs
@@ -0,0 +1,18 @@
+﻿using Azure;
+using System;
+using System.IO;
+
+namespace PurviewAutomation.Utility
+{
+    internal static class Utils
+    {
+        internal static BinaryData GetContentFromResponse(Response r)
+        {
+            // Workaround azure/azure-sdk-for-net#21048, which prevents .Content from working when dealing with responses
+            // from the playback system.
+            MemoryStream ms = new MemoryStream();
+            r.ContentStream.CopyTo(ms);
+            return new BinaryData(ms.ToArray());
+        }
+    }
+}
