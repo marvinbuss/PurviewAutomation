f30a626652d319be631f972ef21fdbe60cded521
Marvin Buss <marvin.buss@gmail.com>
2021-12-22 00:17:37 +0100

    added synapse role defs

---
+++ code/PurviewAutomation/Models/General/SynapseRole.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PurviewAutomation.Models.General;
+
+internal enum SynapseRole
+{
+    Administrator,
+    SqlAdministrator,
+    ApacheSparkAdministrator,
+    Contributor,
+    ArtifactPublisher,
+    ArtifactUser,
+    ComputeOperator,
+    CredentialUser,
+    LinkedDataManager,
+    User
+}
+
+internal static class SynapseRoleConverter
+{
+    internal static Guid ConvertRoleToGuid(SynapseRole role)
+    {
+        return role switch
+        {
+            SynapseRole.Administrator => new Guid(""),
+            SynapseRole.SqlAdministrator => new Guid(""),
+            SynapseRole.ApacheSparkAdministrator => new Guid(""),
+            SynapseRole.Contributor => new Guid(""),
+            SynapseRole.ArtifactPublisher => new Guid(""),
+            SynapseRole.ArtifactUser => new Guid(""),
+            SynapseRole.ComputeOperator => new Guid(""),
+            SynapseRole.CredentialUser => new Guid(""),
+            SynapseRole.LinkedDataManager => new Guid(""),
+            SynapseRole.User => new Guid(""),
+            _ => throw new ArgumentException(message: "Incorrect role provided", paramName: nameof(role)),
+        };
+    }
+}
