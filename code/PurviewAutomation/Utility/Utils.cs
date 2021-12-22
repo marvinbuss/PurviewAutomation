07d02e4d21cf8c7cf6c0608ad21c1e319eea1d66
Marvin Buss <marvin.buss@gmail.com>
2021-12-22 18:27:54 +0100

    updated utils

--- code/PurviewAutomation/Utility/Utils.cs
+++ code/PurviewAutomation/Utility/Utils.cs
@@ -2,17 +2,16 @@
 using System;
 using System.IO;
 
-namespace PurviewAutomation.Utility
+namespace PurviewAutomation.Utility;
+
+internal static class Utils
 {
-    internal static class Utils
+    internal static BinaryData GetContentFromResponse(Response r)
     {
-        internal static BinaryData GetContentFromResponse(Response r)
-        {
-            // Workaround azure/azure-sdk-for-net#21048, which prevents .Content from working when dealing with responses
-            // from the playback system.
-            MemoryStream ms = new MemoryStream();
-            r.ContentStream.CopyTo(ms);
-            return new BinaryData(ms.ToArray());
-        }
+        // Workaround azure/azure-sdk-for-net#21048, which prevents .Content from working when dealing with responses
+        // from the playback system.
+        var ms = new MemoryStream();
+        r.ContentStream.CopyTo(ms);
+        return new BinaryData(ms.ToArray());
     }
 }
