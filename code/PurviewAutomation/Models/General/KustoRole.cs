da76334750eefb4375e4896f5c63661a57d9040c
Marvin Buss <marvin.buss@gmail.com>
2021-12-23 23:57:04 +0100

    Added Kusto role class

---
+++ code/PurviewAutomation/Models/General/KustoRole.cs
@@ -0,0 +1,22 @@
+﻿namespace PurviewAutomation.Models.General;
+
+internal enum KustoRole
+{
+    AllDatabasesAdmin,
+    AllDatabasesViewer,
+    AllDatabasesMonitor
+}
+
+internal static class KustoRoleConverter
+{
+    internal static string ConvertRoleToString(KustoRole role)
+    {
+        return role switch
+        {
+            KustoRole.AllDatabasesAdmin => "AllDatabasesAdmin",
+            KustoRole.AllDatabasesViewer => "AllDatabasesViewer",
+            KustoRole.AllDatabasesMonitor => "AllDatabasesMonitor",
+            _ => null,
+        };
+    }
+}
